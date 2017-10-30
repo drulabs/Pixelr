f96ccaade52fcd77725aeba344ea81d8315fc3ce
drulabs <kaushal.devil009@gmail.com>
2017-10-30 19:00:36 +0530

    Starter activity complete

--- app/src/main/java/org/drulabs/pixelr/screens/notes/UserNotes.java
+++ app/src/main/java/org/drulabs/pixelr/screens/notes/UserNotes.java
@@ -5,55 +5,43 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
 import org.drulabs.pixelr.R;
 import org.drulabs.pixelr.dto.NoteDTO;
-import org.drulabs.pixelr.utils.Store;
 
 import java.util.Date;
 
 public class UserNotes extends AppCompatActivity implements View.OnClickListener {
 
-    DatabaseReference notesRef;
+
     private RecyclerView rvUserNotes;
     private EditText etUserNote;
     private FirebaseRecyclerAdapter adapter;
+    // DatabaseReference notesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_notes);
-        notesRef = FirebaseDatabase.getInstance().getReference().child("users").child(Store
-                .getInstance(this).getMyKey()).child("notes");
+        // Initiate notes db ref here
         initializeUI();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (adapter != null) {
-            adapter.startListening();
-        }
+        // Start listening to real time database
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (adapter != null) {
-            adapter.stopListening();
-        }
+        // Stop listening to real time database
     }
 
     private void initializeUI() {
@@ -68,28 +56,8 @@ public class UserNotes extends AppCompatActivity implements View.OnClickListener
     }
 
     FirebaseRecyclerAdapter createFirebaseUiAdapter() {
-        Query query = notesRef.limitToLast(20);
-        FirebaseRecyclerOptions<NoteDTO> options = new FirebaseRecyclerOptions.Builder<NoteDTO>()
-                .setQuery(query, NoteDTO.class)
-                .build();
-        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<NoteDTO, NoteHolder>(options) {
-
-            @Override
-            protected void onBindViewHolder(NoteHolder holder, int position, NoteDTO model) {
-                holder.bind(model);
-            }
-
-            @Override
-            public NoteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                // Create a new instance of the ViewHolder, in this case we are using a custom
-                // layout called R.layout.message for each item
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note,
-                        parent, false);
-                return new NoteHolder(view);
-            }
-        };
-
-        return adapter;
+        // create firebase adapter
+        return null;
     }
 
     @Override
@@ -106,9 +74,7 @@ public class UserNotes extends AppCompatActivity implements View.OnClickListener
                 noteDTO.setNoteText(noteText);
                 noteDTO.setTimestamp(System.currentTimeMillis());
                 noteDTO.setAddedOn((new Date()).toString());
-                notesRef.push().setValue(noteDTO).addOnSuccessListener(aVoid -> etUserNote
-                        .setText(""));
-
+                //Push the note to database
                 break;
             default:
                 break;
